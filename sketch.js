fa26571468df22d292e8d4a476c0258f029f0949
Spixxr <44309126+Spixxr@users.noreply.github.com>
2020-01-27 12:29:23 -0600

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var back
+var purple
+var blue
+var green
+var orange
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  back    = color("#031927")
+  purple  = color("#5F0F40")
+  blue    = color("#4DCCBD")
+  green   = color("#00BA31")
+  orange  = color("#FF7B16")
+}
+
+function draw() {
+  background(back);
+}
